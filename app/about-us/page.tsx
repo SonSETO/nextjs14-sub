531a12c2b095cd82ecf30b7eb0308fadbe10b3db
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-03-31 03:42:43 +0900

    add to aboutus

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,10 +1,12 @@
+import styles from "../../styles/aboutus.module.css";
+
 export const metadata = {
   title: "About us",
 };
 
 export default function AboutUs() {
   return (
-    <div>
+    <div className={styles.contents}>
       <h1>
         아 진짜 뭔가 쉬우면서도 겁나 어렵고 하기는 싫은데 재미는 있고 그렇다고
         뭔가 쏙쏙 잘되는 것도 아니고 너무 열받앙
--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,3 @@
 export const API_URL = "https://books-api.nomadcoders.workers.dev/lists";
+
+// export const API_URL2 = "https://books-api.nomadcoders.workers.dev/list?name=";
